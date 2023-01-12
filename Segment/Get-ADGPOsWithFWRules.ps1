ee76b36b8bd316923a73d5c424cea8f791029421
SecurityJedi <nicholas.dicola@outlook.com>
2023-01-12 08:54:10 -0500

    Create Get-ADGPOsWithFWRules.ps1

---
+++ Segment/Get-ADGPOsWithFWRules.ps1
@@ -0,0 +1,19 @@
+$adDomain = Get-ADDomain
+
+$rules = New-object "System.Collections.Generic.Dictionary[[String],[String]]"
+$gpos = Get-GPO -Domain $domain.DNSRoot -All
+foreach($gpo in $GPOs){
+    if(($GPO.DisplayName) -eq "ZeroNetworksMonitor"){}
+    elseif(($gpo.DisplayName) -eq "ZeroNetworksProtect"){}
+    else{
+        Write-host "Checking " $gpo.DisplayName 
+        $result = Get-GPRegistryValue -Name $gpo.DisplayName -Key "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\FirewallRules" -ErrorAction SilentlyContinue | where {$_.Value -like "*Dir=In*"} 
+        if($result -ne $null){
+            $rules.Add($gpo.Displayname,$result.Value)
+        }
+    }
+}
+#$rules | FL
+
+Write-Host "GPO Names with Firewall Rules"
+$rules.Keys
