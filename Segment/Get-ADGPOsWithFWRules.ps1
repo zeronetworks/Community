b5cb91b5638edf6e60fee30a73c148a7815a4aab
SecurityJedi <nicholas.dicola@outlook.com>
2023-03-08 07:57:21 -0500

    Update Get-ADGPOsWithFWRules.ps1

--- Segment/Get-ADGPOsWithFWRules.ps1
+++ Segment/Get-ADGPOsWithFWRules.ps1
@@ -8,20 +8,31 @@
 Import-Module ActiveDirectory, GroupPolicy
 $adDomain = Get-ADDomain
 
-$rules = New-object "System.Collections.Generic.Dictionary[[String],[String]]"
+$policies = New-object "System.Collections.Generic.Dictionary[[String],[String]]"
 $gpos = Get-GPO -Domain $domain.DNSRoot -All
 foreach($gpo in $GPOs){
+    $found = $false
     if(($GPO.DisplayName) -eq "ZeroNetworksMonitor"){}
     elseif(($gpo.DisplayName) -eq "ZeroNetworksProtect"){}
     else{
         Write-host "Checking " $gpo.DisplayName 
-        $result = Get-GPRegistryValue -Name $gpo.DisplayName -Key "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\FirewallRules" -ErrorAction SilentlyContinue | where {$_.Value -like "*Dir=In*"} 
-        if($result -ne $null){
-            $rules.Add($gpo.Displayname,$result.Value)
+        $result = Get-GPRegistryValue -Name $gpo.DisplayName -Key "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\FirewallRules" -ErrorAction SilentlyContinue
+        if($result -ne $null){$found = $true}
+        $result = Get-GPRegistryValue -Name $gpo.DisplayName -Key "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\DomainProfile" -ErrorAction SilentlyContinue
+        if($result -ne $null){$found = $true}
+        $result = Get-GPRegistryValue -Name $gpo.DisplayName -Key "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\PrivateProfile" -ErrorAction SilentlyContinue
+        if($result -ne $null){$found = $true}
+        $result = Get-GPRegistryValue -Name $gpo.DisplayName -Key "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\PublicProfile" -ErrorAction SilentlyContinue
+        if($result -ne $null){$found = $true}
+
+        if($found -eq $true){
+            $policies.Add($gpo.Displayname,"")
         }
     }
 }
 #$rules | FL
 
-Write-Host "GPO Names with Firewall Rules"
-$rules.Keys
+
+Write-Host "GPO Names with Firewall Settings" -ForegroundColor Red
+$policies.Keys
+
