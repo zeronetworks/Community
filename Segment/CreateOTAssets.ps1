55ce5a65171c65d8a63f5d3718ba9bb1b5399ad6
SecurityJedi <nicholas.dicola@outlook.com>
2023-05-10 10:18:10 -0400

    Update CreateOTAssets.ps1
    add ot asset types

--- Segment/CreateOTAssets.ps1
+++ Segment/CreateOTAssets.ps1
@@ -4,6 +4,35 @@
     .Description
     This example will loop through OT assets in a provided CSV file and then add each individual asset to Zero Networks OT/IoT asset group.
 #>
+# OT Asset Types
+# 'IP camera' = 4
+# 'Smart TV' = 5
+# 'Factory controller' = 6
+# 'Medical device' = 7
+# 'Printer' = 8
+# 'Scanner' = 9
+# 'Smart card reader' = 10
+# 'Router' = 11
+# 'Hypervisor' = 12
+# 'PLC' = 13
+# 'HMI' = 14
+# 'Switch' = 15
+# 'Terminal station' = 16
+# 'RTU' = 17
+# 'Wireless access point' = 18
+# 'Historian' = 19
+# 'Game console' = 20
+# 'Fire alarm' = 21
+# 'UPS' = 22
+# 'Storage appliance' =23
+# 'Virtualization appliance' = 24
+# 'Firewall appliance' = 25
+# 'Security scanner' = 26
+# 'Security controller' = 27  
+# 'Door lock' = 28
+# 'Biometric entry system' = 29
+# 'HVAC' = 30
+# 'Room scheduler' = 31
 
 $apikey = "<apiKey>"
 
