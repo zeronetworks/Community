5489c912136cb021196c6115a005d6f96e8af8ed
kenrward <kenrward@gmail.com>
2023-07-19 14:51:32 -0400

    outbound block url

---
+++ Segment/Rules/Update-ZNOutboundBlockfromURLFile.ps1
@@ -0,0 +1,64 @@
+#Get API key from settings and paste here
+$APIKey = $Env:ZNAPIKey
+
+#Get the block rule id from the portal and paste here
+$blockRuleId = "0c649ffa-1c89-480a-a791-aa1d82166a49"
+
+#Get the filepath of bad URLs
+$badURLFilePath = "c:\tmp\badurls.txt"
+
+#Headers
+$znHeaders = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$znHeaders.Add("Authorization",$APIKey)
+$znHeaders.Add("Accept","application/json")
+
+$uri = "https://portal.zeronetworks.com/api/v1"
+
+$blockedURLs = @()
+
+#Get the block rule
+$blockRule = (Invoke-RestMethod -Uri "$uri/protection/rules/outbound-block/$blockRuleId" -Method Get -ContentType application/json -Headers $znHeaders).items
+
+#Get the bad URLs from file
+$badURLs = Get-Content $badURLFilePath
+
+foreach($url in $badURLs){
+    #check for wildcard
+    if($url.StartsWith("*")){
+        $blockedURLs += "b:18$url"
+    } else {
+        $blockedURLs += "b:17$url"
+    }
+}
+
+
+
+#set the updated rule properties
+$updatedBlockRule = @{
+    "description"= $blockRule.description
+    "expiresAt" = $blockRule.expiresAt
+    "localEntityId" =  $blockRule.localEntityId
+    "localProcessesList"= @(
+      "*"
+    )
+    "portsList" = @(
+        $blockRule.portsList
+    )
+    "remoteEntityIdsList" = @($blockedURLs)
+    "state" = $blockRule.state
+  }
+
+  #Update the rule
+Invoke-RestMethod -Uri "$uri/protection/rules/outbound-block/$blockRuleId" -Method PUT -Body ($updatedBlockRule | ConvertTo-Json) -Headers $znHeaders -ContentType application/json
+
+
+
+
+
+
+
+
+
+
+
+
