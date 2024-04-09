721083e4f2a92596939d56fea6dbe74270ccf113
Ken Ward <ken.ward@zeronetworks.com>
2024-04-09 14:53:22 -0400

    Update breakglass-single.ps1

--- Segment/Trust Server/breakglass-single.ps1
+++ Segment/Trust Server/breakglass-single.ps1
@@ -3,4 +3,7 @@ $Credential = Get-Credential
 $ps = New-PSSession -ComputerName $AssetFQDN -Credential $Credential -Authentication Kerberos -ErrorAction SilentlyContinue -ErrorVariable errmsg
 
 Invoke-Command -Session $ps -Scriptblock { netsh advfirewall firewall add rule name="Zero Networks Break Glass Inbound Allow All" dir=in action=allow protocol=any } -ErrorAction Stop -ErrorVariable errmsg | Out-Null
+## UNCOMMENT for RDP Only, make sure to comment out line above.
+#Invoke-Command -Session $ps -Scriptblock { netsh advfirewall firewall add rule name="Zero Networks Break Glass Inbound Allow RDP" dir=in action=allow protocol=TCP localport=3389 } -ErrorAction Stop -ErrorVariable errmsg | Out-Null
+
 Invoke-Command -Session $ps -Scriptblock { netsh advfirewall firewall add rule name="Zero Networks Break Glass Outbound Allow All" dir=out action=allow protocol=any } -ErrorAction Stop -ErrorVariable errmsg | Out-Null
