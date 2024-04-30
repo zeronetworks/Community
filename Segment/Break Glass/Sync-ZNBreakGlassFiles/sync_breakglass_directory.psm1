5d3725e20ead2377caa1e68d14510053aea71cfe
Thomas Obarowski <tjobarow@gmail.com>
2024-04-30 12:21:29 -0400

    Making sure help is accurate

--- Segment/Break Glass/Sync-ZNBreakGlassFiles/sync_breakglass_directory.psm1
+++ Segment/Break Glass/Sync-ZNBreakGlassFiles/sync_breakglass_directory.psm1
@@ -21,6 +21,19 @@ SEE .EXAMPLES SECTION FOR USAGE
     a remote directory provided at run time. It then closes the pssession and removes the 
     local copy of BreakGlass.zip
 
+.PARAMETER Credential
+    PSCredential object needed to create PSSession to Segment Server
+.PARAMETER Username
+    Account username to use to create PSSession
+.PARAMETER Password
+    Password for the associated username provided
+.PARAMETER SegmentServer
+    SHORT hostname for Segment Server (such as znserver)
+.PARAMETER RemoteDirectory
+    Remote directory to copy the archive into
+.PARAMETER BreakGlassServer
+    SHORT hostname for the BreakGlass (destination) server
+
 .NOTES
     DEPENDENCIES:
     * Inbound allow rule allowing the device running this script to WinRM to both
@@ -52,22 +65,23 @@ SEE .EXAMPLES SECTION FOR USAGE
         Sync-BreakGlassDirectory -Username $ENV:username -Password $ENV:password -BreakGlassServer $ENV:BreakGlassServer -SegmentServer $ENV:SegmentServer -RemoteDirectory $ENV:RemoteDirectory
 #>
 
-<#
-.SYNOPSIS
-    Log-Message function writes a string to stdout and appends to a pre-named log file.
-.DESCRIPTION
-    Accepts a string, adds a timestamp infront of said string, writes to stdout, and then
-    appends to a log file. less verbose than Start-Transcript, but still includes timestamps
-.PARAMETER Message
-    String message to log to stdout and file
-.OUTPUTS
-    None
-.NOTES
-    N/A
-.FUNCTIONALITY
-   Logging Mechanism
-#>
+
 function Log-Message {
+    <#
+    .SYNOPSIS
+        Log-Message function writes a string to stdout and appends to a pre-named log file.
+    .DESCRIPTION
+        Accepts a string, adds a timestamp infront of said string, writes to stdout, and then
+        appends to a log file. less verbose than Start-Transcript, but still includes timestamps
+    .PARAMETER Message
+        String message to log to stdout and file
+    .OUTPUTS
+        None
+    .NOTES
+        N/A
+    .FUNCTIONALITY
+    Logging Mechanism
+    #>
     param(
         [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
         [string]$Message = ""
@@ -76,24 +90,25 @@ function Log-Message {
     "$((Get-Date -Format 'yyyy-MM-dd HH:mm:ss').ToString()) - $Message" | Out-File -FilePath "$((Get-Date -Format 'yyyy-MM-dd').ToString())_sync-BreakGlass-Directory.log" -Append
 }
 
-<#
-.SYNOPSIS
-    Get-Credentials - Creates a PSCredential Object based on provided username/password
-.DESCRIPTION
-    Accepts username and password, converts the password to a securestring, and then
-    creates a PSCredentialObject based on the securestring and username
-.PARAMETER Username
-    Admin account username
-.PARAMETER Password
-    Admin account password
-.OUTPUTS
-    Return PSCredentials object
-.NOTES
-    Information or caveats about the function e.g. 'This function is not supported in Linux'
-.FUNCTIONALITY
-   Create PSCredential object used in other Cmdlets
-#>
+
 function Get-Credentials {
+    <#
+    .SYNOPSIS
+        Get-Credentials - Creates a PSCredential Object based on provided username/password
+    .DESCRIPTION
+        Accepts username and password, converts the password to a securestring, and then
+        creates a PSCredentialObject based on the securestring and username
+    .PARAMETER Username
+        Admin account username
+    .PARAMETER Password
+        Admin account password
+    .OUTPUTS
+        Return PSCredentials object
+    .NOTES
+        Information or caveats about the function e.g. 'This function is not supported in Linux'
+    .FUNCTIONALITY
+    Create PSCredential object used in other Cmdlets
+    #>
     param(
         [Parameter(Mandatory = $true)]
         [string]$Username,
@@ -116,31 +131,32 @@ function Get-Credentials {
     }
 }
 
-<#
-.SYNOPSIS
-    Get-BreakGlassZip - Compresses and copies the contents of 
-    C:\Program Files\Zero Networks\BreakGlass\ on the Segment Server
-    to your local filesystem
-.DESCRIPTION
-    This function requires a valid credential object that has local administrator
-    privileges on the ZN Segment Server, as well as the ComputerName of the Segment
-    Server. It then creates a new PSSession with the Segment Server. It uses
-    Invoke-Command to compress the C:\Program Files\Zero Networks\BreakGlass\ directory
-    to a file named BreakGlass.zip. It then uses Copy-Item -FromSession to copy
-    the BreakGlass.zip from the Segment Server PSSession into the local filesystem
-    where the script is ran from. It then terminates the PSSession to the Segment
-    Server.
-.PARAMETER Credential
-    PSCredential object needed to create PSSession to Segment Server
-.PARAMETER SegmentServer
-    SHORT hostname for Segment Server (such as znserver)
-.OUTPUTS
-    N/A - None
-.NOTES
-    The credentials object used must be enabled for PSRemoting on the segment server
-    and have local admin.
-#>
+
 function Get-BreakGlassZip {
+    <#
+    .SYNOPSIS
+        Get-BreakGlassZip - Compresses and copies the contents of 
+        C:\Program Files\Zero Networks\BreakGlass\ on the Segment Server
+        to your local filesystem
+    .DESCRIPTION
+        This function requires a valid credential object that has local administrator
+        privileges on the ZN Segment Server, as well as the ComputerName of the Segment
+        Server. It then creates a new PSSession with the Segment Server. It uses
+        Invoke-Command to compress the C:\Program Files\Zero Networks\BreakGlass\ directory
+        to a file named BreakGlass.zip. It then uses Copy-Item -FromSession to copy
+        the BreakGlass.zip from the Segment Server PSSession into the local filesystem
+        where the script is ran from. It then terminates the PSSession to the Segment
+        Server.
+    .PARAMETER Credential
+        PSCredential object needed to create PSSession to Segment Server
+    .PARAMETER SegmentServer
+        SHORT hostname for Segment Server (such as znserver)
+    .OUTPUTS
+        N/A - None
+    .NOTES
+        The credentials object used must be enabled for PSRemoting on the segment server
+        and have local admin.
+    #>
     param(
         [Parameter(Mandatory = $true)]
         [System.Management.Automation.PSCredential]$Credential,
@@ -180,36 +196,37 @@ function Get-BreakGlassZip {
     }
 }
 
-<#
-.SYNOPSIS
-    Push-BreakGlassZip - Copies the BreakGlass.zip archive from local folder into the
-    the RemoteDirectory specified on the breakglass (destination) server
-.DESCRIPTION
-    This function requires a valid credential object that has local administrator
-    privileges on the BreakGlass Server, as well as the ComputerName of the Segment
-    Server, ComputerName of the BreakGlass Server, and Remote Directory where the file
-    will be copied to. It then creates a new PSSession with the BreakGlass Server. It uses
-    Invoke-Command to verify the remote directory specified exists, and tries to create
-    it if it does not exist. It then uses Copy-Item -ToSession to copy
-    the BreakGlass.zip from the local filesystem into the BreakGlass server, at the
-    provided remote directory. It renames BreakGlass.zip to include the Segment Server
-    name the archive was retrieved from. It then terminates the PSSession to the Segment
-    Server.
-.PARAMETER Credential
-    PSCredential object needed to create PSSession to Segment Server
-.PARAMETER SegmentServer
-    SHORT hostname for Segment Server (such as znserver)
-.PARAMETER RemoteDirectory
-    Remote directory to copy the archive into
-.PARAMETER BreakGlassServer
-    SHORT hostname for the BreakGlass (destination) server
-.OUTPUTS
-    N/A - None
-.NOTES
-    The credentials object used must be enabled for PSRemoting on the breakglass
-    server and have local admin.
-#>
+
 function Push-BreakGlassZip {
+    <#
+    .SYNOPSIS
+        Push-BreakGlassZip - Copies the BreakGlass.zip archive from local folder into the
+        the RemoteDirectory specified on the breakglass (destination) server
+    .DESCRIPTION
+        This function requires a valid credential object that has local administrator
+        privileges on the BreakGlass Server, as well as the ComputerName of the Segment
+        Server, ComputerName of the BreakGlass Server, and Remote Directory where the file
+        will be copied to. It then creates a new PSSession with the BreakGlass Server. It uses
+        Invoke-Command to verify the remote directory specified exists, and tries to create
+        it if it does not exist. It then uses Copy-Item -ToSession to copy
+        the BreakGlass.zip from the local filesystem into the BreakGlass server, at the
+        provided remote directory. It renames BreakGlass.zip to include the Segment Server
+        name the archive was retrieved from. It then terminates the PSSession to the Segment
+        Server.
+    .PARAMETER Credential
+        PSCredential object needed to create PSSession to Segment Server
+    .PARAMETER SegmentServer
+        SHORT hostname for Segment Server (such as znserver)
+    .PARAMETER RemoteDirectory
+        Remote directory to copy the archive into
+    .PARAMETER BreakGlassServer
+        SHORT hostname for the BreakGlass (destination) server
+    .OUTPUTS
+        N/A - None
+    .NOTES
+        The credentials object used must be enabled for PSRemoting on the breakglass
+        server and have local admin.
+    #>
     param(
         [Parameter(Mandatory = $true)]
         [System.Management.Automation.PSCredential]$Credential,
@@ -264,38 +281,39 @@ function Push-BreakGlassZip {
     }
 }
 
-<#
-.SYNOPSIS
-    Sync-BreakGlassDirectory - Main function that executes scripts - calls required
-    functions to copy archive from segment server to breakglass server
-.DESCRIPTION
-    This function accepts either a PSCredential object directly, or a username/password,
-    and then determines which was provided. If user/pass, it calls Get-Credential to 
-    convert that to a PSCredential object. It then executes:
-    1. Get-BreakGlassZip
-    2. Push-BreakGlassZip
-    Which copies the BreakGlass directory from the Segement Server to the BreakGlass server
-.PARAMETER Credential
-    PSCredential object needed to create PSSession to Segment Server
-.PARAMETER Username
-    Account username to use to create PSSession
-.PARAMETER Password
-    Password for the associated username provided
-.PARAMETER SegmentServer
-    SHORT hostname for Segment Server (such as znserver)
-.PARAMETER RemoteDirectory
-    Remote directory to copy the archive into
-.PARAMETER BreakGlassServer
-    SHORT hostname for the BreakGlass (destination) server
-.OUTPUTS
-    N/A - None
-.NOTES
-    You can provide either a PSCredential object such as using (Get-Credential)
-    in the function call. OR you can provide it a username/password
-.FUNCTIONALITY
-   Basically the __init__ function of this script
-#>
+
 function Sync-BreakGlassDirectory {
+    <#
+    .SYNOPSIS
+        Sync-BreakGlassDirectory - Main function that executes scripts - calls required
+        functions to copy archive from segment server to breakglass server
+    .DESCRIPTION
+        This function accepts either a PSCredential object directly, or a username/password,
+        and then determines which was provided. If user/pass, it calls Get-Credential to 
+        convert that to a PSCredential object. It then executes:
+        1. Get-BreakGlassZip
+        2. Push-BreakGlassZip
+        Which copies the BreakGlass directory from the Segement Server to the BreakGlass server
+    .PARAMETER Credential
+        PSCredential object needed to create PSSession to Segment Server
+    .PARAMETER Username
+        Account username to use to create PSSession
+    .PARAMETER Password
+        Password for the associated username provided
+    .PARAMETER SegmentServer
+        SHORT hostname for Segment Server (such as znserver)
+    .PARAMETER RemoteDirectory
+        Remote directory to copy the archive into
+    .PARAMETER BreakGlassServer
+        SHORT hostname for the BreakGlass (destination) server
+    .OUTPUTS
+        N/A - None
+    .NOTES
+        You can provide either a PSCredential object such as using (Get-Credential)
+        in the function call. OR you can provide it a username/password
+    .FUNCTIONALITY
+    Basically the __init__ function of this script
+    #>
     param(
         [Parameter(Mandatory = $false)]
         [System.Management.Automation.PSCredential]$Credential = $null,
@@ -334,5 +352,4 @@ function Sync-BreakGlassDirectory {
     Push-BreakGlassZip -Credential $Credential -RemoteDirectory $RemoteDirectory -BreakGlassServer $BreakGlassServer -SegmentServer $SegmentServer
     Log-Message "Completed copying BreakGlass directory from $SegmentServer to $BreakGlassServer at $RemoteDirectory\BreakGlass-From-$($SegmentServer)"
 }
-
 
