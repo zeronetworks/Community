46d4bbc6c55e0f810f7593d18df2f90dafe83bca
Olaf Gradin <olaf.gradin@gmail.com>
2025-02-28 11:49:48 -0500

    Revert "Rewrite for Powershell and pwsh Core support"
    This reverts commit c0edeb85184fabeda0a5bf50477e054772b94531.

--- Segment/MFA Push/getSecretMicrosoftAuth.ps1
+++ Segment/MFA Push/getSecretMicrosoftAuth.ps1
@@ -1,82 +1,25 @@
-[CmdletBinding()]
-param (
-    [Parameter()]
-    [string] $TenantId
-)
+# Install the AzureAD module if not already installed
+if (-not (Get-Module -ListAvailable -Name AzureAD)) {
+    Install-Module -Name AzureAD -Force
+}
 
-$IsElevated = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+# Import the AzureAD module
+Import-Module AzureAD
+
+# Connect to AzureAD
+Connect-AzureAD
+
+# Define the Azure Multi-Factor Authentication (MFA) App ID
+$AzureMFAAppID = "981f26a1-7f43-403b-a875-f8b09b8cd720"
+
+# Get the AzureAD service principal ObjectId for the MFA App ID
+$AzureMFAObjID = Get-AzureADServicePrincipal -Filter "AppId eq '$AzureMFAAppID'" | Select-Object -ExpandProperty ObjectId
 
 # Set the end date for the ClientSecret
 $endDate = (Get-Date).AddYears(2)
 
-# Pwsh Core flavor because the module is different
-if ($PSVersionTable.PSEdition -eq "Core") {
-
-    if (-Not (Get-Module -ListAvailable -Name Az)) {
-        if ($IsElevated) {
-            Install-Module -Name Az -AllowClobber -Force
-        } else {
-            Write-Host -ForegroundColor Red "AzureAD Module is not installed and you are not running in an elevated session."
-            Write-Host -ForegroundColor Red "Please run Powershell as an Administrator and try again."
-            return
-        }
-        
-    }
-
-    if (-Not (Get-AzContext).Account | Out-Null) {
-        $login = "Connect-AzAccount"
-        if ($TenantId) { $login += " -Tenant $($TenantId)" }
-        else { write-warning "skipping Tenant specificity on connecting to AzureAD"}
-        Invoke-Expression $login | Out-Null
-    }
-
-    # Get the Azure Multi-Factor Authentication (MFA) App ID from Zero Networks Enterprise App (Gallery Entity)
-    $AzureMFAAppID = (Get-AzADServicePrincipal -DisplayNameBeginsWith 'zero networks').AppId
-
-    # Get the AzureAD service principal ObjectId for the MFA App ID
-    $AzureMFAObj = Get-AzADServicePrincipal -ApplicationId $AzureMFAAppID
-
-    # Generate a new secret and set its expiry based on $endDate variable
-    $ClientSecret = New-AzADSpCredential -ObjectId $AzureMFAObj.Id -EndDate $endDate | Select-Object -ExpandProperty secretText
-
-
-} else {
-    if (-not (Get-Module -ListAvailable -Name AzureAD)) {
-        if ($IsElevated) {
-            Install-Module -Name AzureAD -Repository PSGallery -AllowClobber -Force
-        } else {
-            Write-Host -ForegroundColor Red "AzureAD Module is not installed and you are not running in an elevated session."
-            Write-Host -ForegroundColor Red "Please run Powershell as an Administrator and try again."
-            return
-        }
-    }
-
-    # Connect to AzureAD
-    try {Get-AzureADTenantDetail | Out-Null}
-    catch {
-        $login = "Connect-AzureAD"
-        
-        if ($TenantId) { $login += " -TenantId $($TenantId)" }
-        else { write-warning "skipping Tenant specificity on connecting to AzureAD"}
-        
-        Invoke-Expression $login | Out-Null
-    }
-
-    # Get the Azure Multi-Factor Authentication (MFA) App ID from Zero Networks Enterprise App (Gallery Entity)
-    $AzureMFAAppID = (Get-AzureADServicePrincipal -Filter "DisplayName eq 'Zero Networks'").AppId
-
-    # Get the AzureAD service principal ObjectId for the MFA App ID
-    $AzureMFAObjID = Get-AzureADServicePrincipal -Filter "AppId eq '$AzureMFAAppID'" | Select-Object -ExpandProperty ObjectId
-
-    # Generate a new secret and set its expiry based on $endDate variable
-    $ClientSecret = New-AzureADServicePrincipalPasswordCredential -ObjectId $AzureMFAObjID -EndDate $endDate | Select-Object -ExpandProperty Value
-}
+# Generate a new ClientSecret and retrieve the value
+$ClientSecret = New-AzureADServicePrincipalPasswordCredential -ObjectId $AzureMFAObjID -EndDate $endDate | Select-Object -ExpandProperty Value
 
 # Print the ClientSecret
-if ($ClientSecret) {
-    Write-Host "ClientSecret: $($ClientSecret)" -ForegroundColor Green
-    Set-Clipboard -Value $ClientSecret
-    Write-Host -ForegroundColor DarkYellow "Copied to the clipboard if your OS supports it!"
-} else {
-    Write-Warning "No secret was found. Could be an authorization issue! Check that you have the proper permissions and that you are connected to the intended Tenant and Subscription context. You can change these using -TenantId and -SubscriptionName."
-}
+Write-Host "ClientSecret: $ClientSecret"
